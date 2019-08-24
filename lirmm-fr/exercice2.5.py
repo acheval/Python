178520d0f21a6b7e44fca1b89ff3fe88757eaac6
Alexis CHEVALLIER <alexischevallier93+git@gmail.com>
2019-08-24 20:11:55 +0200

    rewrote entire exercise

--- lirmm-fr/exercice2.5.py
+++ lirmm-fr/exercice2.5.py
@@ -1,5 +1,45 @@
 #!/bin/python3
 
-L = [ 987, 65, 43, 21, 43, 98, 145 ]
+L = [987, 65, 43, 21, 43, 98, 145]
 
+#Afficher la liste L
+
+print(L)
+
+#Compter le nombre d'occurrences de 43 dans L
+
+print(L.count(43))
+
+#Ajouter le nombre 25 en dernière position ; afficher L
+
+L.append(25)
+print(L)
+
+#Afficher la longueur de la liste
+
+print(len(L))
+
+#Supprimer un élément 43 de L ; afficher L
+
+L.remove(43)
+print(L)
+
+#Refaire 2.
+
+print(L.count(43))
+
+#Renverser l'ordre de L ; afficher L
+
+L.reverse()
+print(L)
+
+#Trier L ; afficher L
+
+L.sort()
+print(L)
+
+#Supprimer le dernier élément ; afficher L
+
+del L[-1]
+print(L)
 
