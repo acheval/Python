ca76b86f7a464d1ef5fb3a9c06d7795e06941742
acheval <45897888+acheval@users.noreply.github.com>
2019-08-30 16:59:34 +0200

    add exercices 4.1.1 and 4.1.2

---
+++ lirmm-fr/exercice4.1.1.py
@@ -0,0 +1,11 @@
+#!/bin/python/
+
+# Reprenez l'exercice de la Boucle for et faites deux nouvelles
+# versions du script pour que le nombre soit saisi avec la commande
+# input,
+
+print("Enter a number: ")
+n = int(input())
+
+for i in range(n):
+    print(i)
---
+++ lirmm-fr/exercice4.1.2.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+# Reprenez l'exercice de la Boucle for et faites deux nouvelles
+# versions du script pour que le nombre soit passé en paramètre.
+
+import sys
+
+n = int(sys.argv[1])
+
+for i in range(n):
+    print(i)
