35257e81d9c730ef32083a970de0388455de6f92
Alexis CHEVALLIER <alexischevallier93+git@gmail.com>
2019-10-23 16:36:04 +0200

    solved exercice06

--- python_exercices/exercice06.py
+++ python_exercices/exercice06.py
@@ -4,3 +4,18 @@
 # 6. Write a Python program to count the number of characters in a string.
 # Sample String : 'google.com' Expected Result : {'o': 3, 'g': 2, '.': 1, 'e':
 # 1, 'l': 1, 'm': 1, 'c': 1}
+
+string = 'google.com'
+
+d = dict()
+
+for letter in string:
+    if letter in d:
+        d[letter] = d[letter]+1
+    else:
+        d[letter] = 1
+
+for key in list(d.keys()):
+    print(key, ": ", d[key])
+
+
