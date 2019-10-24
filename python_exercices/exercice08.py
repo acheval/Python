ce452907cfde39f053b746b3010dea072730cfd9
Alexis CHEVALLIER <alexischevallier93+git@gmail.com>
2019-10-24 11:48:17 +0200

    exercice08.py solved

--- python_exercices/exercice08.py
+++ python_exercices/exercice08.py
@@ -3,5 +3,17 @@
 
 # 8. Write a Python program to count the number of strings where the string
 # length is 2 or more and the first and last character are same from a given
-# list of strings.  Sample List : ['abc', 'xyz', 'aba', '1221'] Expected Result
+# list of strings.  Sample List : y['abc', 'xyz', 'aba', '1221'] Expected Result
 # : 2
+
+list = ['abc', 'xyz', 'aba', '1221']
+
+n = 0
+
+for i in list:
+    if len(i) > 2 :
+        x = i[:1]
+        y = i[-1:]
+        if x == y:
+            n = n + 1
+print(n)
