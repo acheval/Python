e7a1aafb8f4a4723843000b76b713cda0dca1659
Alexis CHEVALLIER <alexischevallier93+git@gmail.com>
2019-10-28 09:17:04 +0100

    solved exercice12.py

--- python_exercices/exercice12.py
+++ python_exercices/exercice12.py
@@ -4,3 +4,11 @@
 # 12. Write a Python program to get a single string from two given strings,
 # separated by a space and swap the first two characters of each string.  Sample
 # String : 'abc', 'xyz' Expected Result : 'xyc abz'
+
+
+string1 = 'abc'
+
+string2 = 'xyz'
+
+
+print(string2[:2] + string1[2] + ' ' + string1[:2] + string2[2])
