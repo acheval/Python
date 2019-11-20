016310293aff5eed4963a5cf3c8848fe57247f70
Alexis CHEVALLIER <alexischevallier93+git@gmail.com>
2019-11-20 18:01:50 +0100

    solved exercice19

--- python_exercices/exercice19.py
+++ python_exercices/exercice19.py
@@ -3,3 +3,14 @@
 
 # 19. Write a Python program to add key to a dictionary.  Sample Dictionary :
 # {0: 10, 1: 20} Expected Result : {0: 10, 1: 20, 2: 30}
+
+
+def add_to_dictionary( x ):
+    dictionary = {0: 10, 1: 20}
+    "This adds to a dictionary"
+    dictionary.update(x)
+    print("Updated dictionary: " + str(dictionary))
+    return
+
+
+add_to_dictionary({2:30, 4:50})
